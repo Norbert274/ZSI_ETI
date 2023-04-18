c9098a747915d37bb74d667102e320c78c151c1c
Norbert274 <88380373+Norbert274@users.noreply.github.com>
2023-04-18 16:08:15 +0200

    Add files via upload

---
+++ Adresaci.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace nclprospekt.Models
+{
+    public class Adresaci
+    {
+       public List<Adresat> adresaci;
+    }
+
+    public class Adresat
+    {
+        [Display(Name="Imie i Nazwisko")]
+        public string Nazwa { get; set; }
+
+        [Display(Name = "Imie")]
+        public string Imie { get; set; }
+
+        [Display(Name = "Nazwisko")]
+        public string Nazwisko { get; set; }
+        public int UserId { get; set; }
+
+        public bool Wybierz { get; set; }
+
+        [Display(Name = "Grupa")]
+        public string GrupaNazwa { get; set; }
+        public int GrupaId { get; set; }
+
+        [Display(Name = "Typ")]
+        public string TypNazwa { get; set; }
+        public int TypId { get; set; }
+
+        [Display(Name = "Wielkosc")]
+        public string WielkoscNazwa { get; set; }
+        public int WielkoscId { get; set; }
+
+    }
+
+    public class AdresatViewModel
+    {
+        public bool Wybierz { get; set; }
+        public int UserId { get; set; }
+        public string Nazwa { get; set; }
+    }
+
+}
